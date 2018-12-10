f7dd3dadd8516b5f9a50e06e45833b80841e0abc
Son <laughlamz@gmail.com>
2018-12-10 23:04:54 +0700

    after 2s, then focus, but if dont have wait 2s -> wrong .. Virtual DOM render Truely DOM

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,39 +1,25 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-
 import './TodoItem.css';
-import check from '../images/check.svg';
-import uncheck from '../images/uncheck.svg';
 
 class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElement = React.createRef();
+
+        setTimeout(() => this.inputElement.current.focus(), 2000);
+
+        // this can't run, error, cause we have to wait virtual DOM -> truely DOM
+        // this.inputElement.current.focus()        
+    }
+
     render() {
-        const { item, onClick } = this.props;
-        let url = uncheck;
-        if (item.isComplete) {
-            url = check;
-        }
-
         return (
-
-            <div className={classNames('TodoItem', {'TodoItem-complete': item.isComplete === true})}>
-                <img onClick={onClick} className="checkImg" src={url} />
-                <p>{this.props.item.tittle}</p>
+            <div className="TodoItem">
+                <input type="text" ref={this.inputElement}/>
             </div>
         );
     }
 }
 
-TodoItem.propTypes = {
-    item: PropTypes.shape({
-        tittle: PropTypes.string.isRequired,        // isRequired depend on coder
-        isComplete: PropTypes.bool.isRequired
-    }),
-    onClick: PropTypes.func
-};
-
-// TodoItem.defaultProps = {
-//     name: 'Test'                                    // if don't have, then default is ...
-// }
 
 export default TodoItem;
